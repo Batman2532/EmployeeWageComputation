e9cca78c73d32f2c0a8795b98441922a0d673202
Batman2532 <73815402+Batman2532@users.noreply.github.com>
2021-04-26 15:49:37 +0530

    added: using switch case

--- src/EmployeeWageBuilder.java
+++ src/EmployeeWageBuilder.java
@@ -5,18 +5,28 @@ public class EmployeeWageBuilder {
 		int present = 1, wage = 0, fullDayHr = 8, wagePerHr = 20, partTime = 2, partTimeHr = 4;
 		
 		double empCheck = Math.floor(Math.random() * 10 % 3);
-		if (empCheck == present) {
-			System.out.println("Employee is present");
-			wage=wagePerHr*fullDayHr;
-			System.out.println(wage + " Daily Wage");
-		}
-		else if (empCheck == partTime) {
-			System.out.println("Employee is present Half Day");
-			wage=wagePerHr*partTimeHr;
-			System.out.println(wage + " Daily Wage");
-		}
-		else
+		
+		int attendance=(int) empCheck;
+		
+		switch(attendance) {
+		
+		case 0:
 			System.out.println("Employee is absent");
+			break;
+		
+		 case 1: 
+			 System.out.println("Employee is present");
+			 wage=wagePerHr*fullDayHr;
+			 System.out.println(wage + " Daily Wage");  
+			 break;  
+		 case 2:
+			 System.out.println("Employee is present Half Day");
+				wage=wagePerHr*partTimeHr;
+				System.out.println(wage + " Daily Wage");
+				break;
+		
+		}
+		
 	}
 
 }
