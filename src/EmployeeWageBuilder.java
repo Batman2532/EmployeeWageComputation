63705d5dba89463fcfc67cdafcff7907eceb5e75
Batman2532 <73815402+Batman2532@users.noreply.github.com>
2021-04-26 15:33:55 +0530

    Added: Part time employee wage calculation added

--- src/EmployeeWageBuilder.java
+++ src/EmployeeWageBuilder.java
@@ -2,17 +2,21 @@
 public class EmployeeWageBuilder {
 
 	public static void main(String[] args) {
-		int present = 1, wage = 0, fullDayHr = 8, wagePerHr = 20;
+		int present = 1, wage = 0, fullDayHr = 8, wagePerHr = 20, partTime = 2, partTimeHr = 4;
 		
-		double empCheck = Math.floor(Math.random() * 10 % 2);
+		double empCheck = Math.floor(Math.random() * 10 % 3);
 		if (empCheck == present) {
 			System.out.println("Employee is present");
 			wage=wagePerHr*fullDayHr;
 			System.out.println(wage + " Daily Wage");
 		}
+		else if (empCheck == partTime) {
+			System.out.println("Employee is present Half Day");
+			wage=wagePerHr*partTimeHr;
+			System.out.println(wage + " Daily Wage");
+		}
 		else
 			System.out.println("Employee is absent");
-		
 	}
 
 }
