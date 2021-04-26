49890dc72c68fa183ae1686c21c08644179dd726
Batman2532 <73815402+Batman2532@users.noreply.github.com>
2021-04-26 15:24:12 +0530

    Added: calculation of daily wages

--- src/EmployeeWageBuilder.java
+++ src/EmployeeWageBuilder.java
@@ -2,10 +2,14 @@
 public class EmployeeWageBuilder {
 
 	public static void main(String[] args) {
-		int present = 1;
+		int present = 1, wage = 0, fullDayHr = 8, wagePerHr = 20;
+		
 		double empCheck = Math.floor(Math.random() * 10 % 2);
-		if (empCheck == present)
+		if (empCheck == present) {
 			System.out.println("Employee is present");
+			wage=wagePerHr*fullDayHr;
+			System.out.println(wage + " Daily Wage");
+		}
 		else
 			System.out.println("Employee is absent");
 		
