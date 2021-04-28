f7e0c353c159ae5ef783726a97c23c0ca48a6dd5
Batman2532 <73815402+Batman2532@users.noreply.github.com>
2021-04-28 19:19:47 +0530

    Refactor: oops concepts added for wage calculation

--- src/EmployeeWageBuilder.java
+++ src/EmployeeWageBuilder.java
@@ -1,10 +1,12 @@
   
 public class EmployeeWageBuilder {
 
+	
+
 	public static void main(String[] args) {
-		int  wage = 0, fullDayHr = 8, wagePerHr = 20, partTimeHr = 4, totalWages=0, workingDays=0,workingHours=0;
+		int fullDayHr = 8, wagePerHr = 20, partTimeHr = 4,  workingDays=0,workingHours=0,totalWages=0, wage=0;
 		
-		while(workingDays == 20 && workingHours == 100) {
+		while(workingDays < 20 && workingHours < 100) {
 		
 		double empCheck = Math.floor(Math.random() * 10 % 3);
 	
@@ -18,21 +20,29 @@ public class EmployeeWageBuilder {
 		
 		 case 1: 
 			 System.out.println("Employee is present");
-			 wage=wagePerHr*fullDayHr; 
+			 wage=calculateDailyWage(wagePerHr, fullDayHr); 
+			 
 			workingDays=workingDays++;
 			workingHours=workingHours+8;
 			 break;  
 		 case 2:
 			 System.out.println("Employee is present Half Day");
-				wage=wagePerHr*partTimeHr;
+			 wage=calculateDailyWage(wagePerHr, partTimeHr);
+			 
 				workingDays=workingDays++;
 				workingHours=workingHours+4;
 				break;
 		}
-		totalWages=totalWages+wage;
-		System.out.println(totalWages + " Monthly Wage");
+		totalWages=totalWages + wage;
+		System.out.println("total salary for month: "+ totalWages);
 		}
+		
+		 
 
 	}
+     	public static int calculateDailyWage(int a, int b) {
+	    	int totalWage = a*b;
+		    return totalWage;
+}
 
 }
