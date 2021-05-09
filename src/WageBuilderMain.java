3a9846da041cbb7d63c5c73be6d1b448042ff138
Batman2532 <73815402+Batman2532@users.noreply.github.com>
2021-05-09 14:05:05 +0530

    Refactor: add multiple company

--- src/WageBuilderMain.java
+++ src/WageBuilderMain.java
@@ -1,11 +1,25 @@
+import java.util.Scanner;
 
 public class WageBuilderMain {
 
 	public static void main(String[] args) {
-		EmployeeWageBuilder empWageBuilder = new EmployeeWageBuilder();		
-		empWageBuilder.addCompanyEmpWage("tata", 100, 20, 120);
-		empWageBuilder.addCompanyEmpWage("Reliance", 100, 21,10);
-		empWageBuilder.calculateWage();	
+		Scanner scanner = new Scanner(System.in);
+		 System.out.println("Welcome to Employee Wedge Builder...");
+	        System.out.println("************************************\n");
+	        int num;
+	        System.out.println("Enter No. of Companies");
+	        num = scanner.nextInt();
+	        
+	        WageCalculationInterface empWageBuilder = new EmployeeWageBuilder();
+	        for (int i = 0; i < num; i++) {
+	            System.out.println(
+	                    "Enter Company Details as given:\n CompanyName EmployeeRatePerHr NoOfWorkingDays MaxHrsPerMonth");
+	            empWageBuilder.addCompanyEmpWage(scanner.next(), scanner.nextInt(), scanner.nextInt(), scanner.nextInt());
+	        }
+	        System.out.println("");
+	        // Method calls
+	        empWageBuilder.calculateWage();
+		
 	}
 
 }
